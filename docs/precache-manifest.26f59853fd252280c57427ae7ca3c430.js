c1eb1076511f2e55ce044e22db853e0b146cda49
gihanrcg <gihanrcg1997@gmail.com>
2023-07-02 10:18:53 +0530

    updated docs

---
+++ docs/precache-manifest.26f59853fd252280c57427ae7ca3c430.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ce8bc89620c05558221610593cb6a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89f7ef8e0aabe449b067",
+    "url": "/static/css/2.aeedae4c.chunk.css"
+  },
+  {
+    "revision": "36d12218ba82e5b6fc7a",
+    "url": "/static/css/main.2e86cc46.chunk.css"
+  },
+  {
+    "revision": "89f7ef8e0aabe449b067",
+    "url": "/static/js/2.84431917.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.84431917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36d12218ba82e5b6fc7a",
+    "url": "/static/js/main.9db08cd8.chunk.js"
+  },
+  {
+    "revision": "75f65fa61937d16649bc",
+    "url": "/static/js/runtime-main.d2b1bba4.js"
+  },
+  {
+    "revision": "92339a20974af6ec69d0361a3bbff09b",
+    "url": "/static/media/12 (1).92339a20.jpg"
+  },
+  {
+    "revision": "be0b1de05f34c7dbc010cb7b412f100c",
+    "url": "/static/media/12 (2).be0b1de0.jpg"
+  },
+  {
+    "revision": "d6076dc825a448fa5c5854611cb7da16",
+    "url": "/static/media/12 (3).d6076dc8.jpg"
+  },
+  {
+    "revision": "2dcdd0294d63ed4b2f17f2b6951b7b59",
+    "url": "/static/media/12 (4).2dcdd029.jpg"
+  },
+  {
+    "revision": "6e92f8b81fb67a75c6ce45cafda368e5",
+    "url": "/static/media/12 (5).6e92f8b8.jpg"
+  },
+  {
+    "revision": "3c3f17adbb8e8a01948a0d76dfe59534",
+    "url": "/static/media/12 (6).3c3f17ad.jpg"
+  },
+  {
+    "revision": "ee492cd6b1c93ea68e360cb4a5b07ee8",
+    "url": "/static/media/12 (7).ee492cd6.jpg"
+  },
+  {
+    "revision": "ea97311deec4078908ea2b716480e1ae",
+    "url": "/static/media/12 (8).ea97311d.jpg"
+  },
+  {
+    "revision": "9c5df3c4d306a2240f9899ca7fa7f106",
+    "url": "/static/media/120554898_2009055415885365_5956423079987255477_o.9c5df3c4.jpg"
+  },
+  {
+    "revision": "8ea4731556035bc7638762d15593c84f",
+    "url": "/static/media/126789784_2119107351546837_1518405151728385913_o.8ea47315.png"
+  },
+  {
+    "revision": "45106619a3302ff765d61d1991f294fa",
+    "url": "/static/media/139709283_3963242583700474_2932390463330137445_o.45106619.jpg"
+  },
+  {
+    "revision": "11d2a35fc88e66fed66376e00ecf4689",
+    "url": "/static/media/144429343_314368053464194_9085205302174898326_o.11d2a35f.jpg"
+  },
+  {
+    "revision": "66654804410be8a26610992d433fab9b",
+    "url": "/static/media/41486278_617028988699144_2494795976527577088_o.66654804.jpg"
+  },
+  {
+    "revision": "4e13a77a37c14103b8ca4b4de5295853",
+    "url": "/static/media/55613120_726260424442666_7850560511774031872_n.4e13a77a.jpg"
+  },
+  {
+    "revision": "88daba7ebebc1eb5d6579154d9147931",
+    "url": "/static/media/55897189_726262351109140_2032770848254853120_o.88daba7e.jpg"
+  },
+  {
+    "revision": "792a97297258521a0a5b7bd86a730a31",
+    "url": "/static/media/89774187_2872124749515244_3824864798827347968_n.792a9729.jpg"
+  },
+  {
+    "revision": "6f672ed9af55f66c29b5e91620136982",
+    "url": "/static/media/89809696_2872124559515263_2552127334593331200_n.6f672ed9.jpg"
+  },
+  {
+    "revision": "ec1e5145696ac8ff1f7d799086cfbd79",
+    "url": "/static/media/99047459_1680898805367696_1548554899568984064_o.ec1e5145.jpg"
+  },
+  {
+    "revision": "778ce50fefd334b7d4c3ecf062ee82ca",
+    "url": "/static/media/AGM1.778ce50f.jpg"
+  },
+  {
+    "revision": "d00443cd9e1f56e74a6e4fa9a7934a49",
+    "url": "/static/media/AGM2.d00443cd.jpg"
+  },
+  {
+    "revision": "bf52e47bbc58614107da407bb2f244ee",
+    "url": "/static/media/blog.bf52e47b.JPG"
+  },
+  {
+    "revision": "e321c6a9f42627259417769215227f51",
+    "url": "/static/media/britishcouncil.e321c6a9.jpg"
+  },
+  {
+    "revision": "b20fa477dd990b05c28e8efb1ff4efad",
+    "url": "/static/media/commitee.b20fa477.jpg"
+  },
+  {
+    "revision": "a7cda3ca009dee946512e6c4cbbc79fb",
+    "url": "/static/media/covid.a7cda3ca.JPG"
+  },
+  {
+    "revision": "92d238ddaf6e9f1a941ef6acb4c9558e",
+    "url": "/static/media/expalining2.92d238dd.jpg"
+  },
+  {
+    "revision": "5e6a0610e0210a109a4131d70c52a25b",
+    "url": "/static/media/explain_miniHackathon.5e6a0610.jpg"
+  },
+  {
+    "revision": "9158410b36a09801d3691a0b5b0f871a",
+    "url": "/static/media/explainingrules.9158410b.jpg"
+  },
+  {
+    "revision": "a9ec526a00d6287376a2939c9833fa24",
+    "url": "/static/media/gihan.a9ec526a.png"
+  },
+  {
+    "revision": "bc741a74a5d2e8d085fa773abb164c2e",
+    "url": "/static/media/hotel.bc741a74.png"
+  },
+  {
+    "revision": "9098efb401a322d8d608e5577e2a2f28",
+    "url": "/static/media/inventory.9098efb4.JPG"
+  },
+  {
+    "revision": "15f02f0f12b9c226f72f5d76eb6d0043",
+    "url": "/static/media/k8.15f02f0f.jpg"
+  },
+  {
+    "revision": "ca388046d05826c0461d078629fa7328",
+    "url": "/static/media/kk.ca388046.jpg"
+  },
+  {
+    "revision": "2e75fdfb06dca57421c9463dbac73dbf",
+    "url": "/static/media/kkk.2e75fdfb.jpg"
+  },
+  {
+    "revision": "5853db0bbf6a1f70dbecdf3761c6d034",
+    "url": "/static/media/mentoring_miniHackathon.5853db0b.jpg"
+  },
+  {
+    "revision": "142ef3fad45964295242c0e41935d6a6",
+    "url": "/static/media/miniHackathon_commitee.142ef3fa.jpg"
+  },
+  {
+    "revision": "a2191803af25dd1d220e12a5ca6a12c7",
+    "url": "/static/media/ms champs participants.a2191803.jpg"
+  },
+  {
+    "revision": "659a26dbae62c2bc3628be3a2e4e41aa",
+    "url": "/static/media/oillamp.659a26db.jpg"
+  },
+  {
+    "revision": "05032f60c93ff3437c20d9db87596ad8",
+    "url": "/static/media/openday.05032f60.jpg"
+  },
+  {
+    "revision": "19e87fd291f1214f6b37b6d1fb09c39b",
+    "url": "/static/media/openday2.19e87fd2.jpg"
+  },
+  {
+    "revision": "306f0d0f56058f12d8f28c61604665b4",
+    "url": "/static/media/schoolmanagementsystem.306f0d0f.JPG"
+  },
+  {
+    "revision": "d6613e2bba9c4d38f3bc7b8d5a556689",
+    "url": "/static/media/step into cloud.d6613e2b.jpg"
+  },
+  {
+    "revision": "c90ba1fc5e86a15472a4738d237f9d73",
+    "url": "/static/media/thuru.c90ba1fc.PNG"
+  },
+  {
+    "revision": "820172011ded80e3e23288569818d491",
+    "url": "/static/media/tokenof_appriciation.82017201.jpg"
+  },
+  {
+    "revision": "9877e9b9fe21a14f43ca5d4e0c2cd98b",
+    "url": "/static/media/zzz.9877e9b9.jpg"
+  },
+  {
+    "revision": "923fefe36d69d11b374517bab7b0acda",
+    "url": "/static/media/zzzz.923fefe3.jpg"
+  },
+  {
+    "revision": "19d18c48447d71cd264767d5e1f583c5",
+    "url": "/static/media/zzzzz.19d18c48.jpg"
+  }
+]);
